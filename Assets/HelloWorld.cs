157f036db2c6e1c82eea8480c669df5ac39bf247
imhiss <dylnbrndn26@gmail.com>
2021-09-01 14:59:59 -0400

    dude what

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelloWorld : MonoBehaviour
+{
+     public Text newText;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown("space"))
+      { 
+          newText.text = "Hello, my name is Dylan Brandon!"; 
+        }
+     
+     if (Input.GetKeyDown("escape"))
+      { 
+         Application.Quit();
+        }
+    
+    
+    }
+
+}
